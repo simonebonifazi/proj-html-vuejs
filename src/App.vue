3304190dc1347ee443a4e8776f0028a71d95206c
simonebonifazi <simonbonifraz@gmail.com>
2022-08-24 09:35:22 +0200

    layout header  demo added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
-    <BaseHeader />
-
+  <main>
+    <!-- <BaseHeader /> -->
+    <header>
+      <div class="d-flex justify-content-between align-items-center ">
+        <figure>
+          <img src="./assets/img/construction_logo.png" alt="logo">
+        </figure>
+        <nav>
+          <span> blabla prova</span>
+          <span> blabla prova</span>
+          <span> blabla prova</span>
+          <span> blabla prova</span>
+          <span> blabla prova</span>
+          <!-- posso creare un componente, si ripete 4 volte
+           ma con testo diverso che mi farò passare di volta in volta da props -->
+          <button type="button"> GET QUOTE</button> 
+        </nav>
+      </div>
+    </header>
     <!-- da mettere in seguito nel componente sopra -->
-    <img src="./assets/img/construction_mobile_logo.png" alt="logo-avada">
-  </div>
+  </main>
 </template>
 
 <script>
-import BaseHeader from './components/BaseHeader.vue';
+// import BaseHeader from './components/BaseHeader.vue';
 
 export default {
   name: 'ConstructionProject',
   components: {
-    BaseHeader
+    // BaseHeader
 }
 }
 </script>
@@ -22,5 +37,17 @@ export default {
 @import "./assets/sass/style.scss";
 @import "~bootstrap/scss/bootstrap";
 
+//Layout
+  section{
+    min-height: 250px;
+    border: 2px dotted gold;
+  }
+div{
+  height: 150px;
+  border: 1px dashed black;
+}
+ span {
+  border: 1.5px solid green;
+ }
 
 </style>
